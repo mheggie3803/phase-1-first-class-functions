a5e328f87c95e195c4fa83307f68f4d5e1a52316
Malcolm Heggie <mheggie@DESKTOP-8IG0D17.localdomain>
2021-08-31 16:15:07 -0400

    commit tree 99744df1dc398d79fc039205eb91edcfc551fd5a

---
+++ index.js
@@ -0,0 +1,11 @@
+const receivesAFunction = (callback) => {
+    return callback();
+};
+
+const returnsANamedFunction = () => {
+     return returnsANamedFunction
+};
+
+const returnsAnAnonymousFunction = function() {
+    return returnsAnAnonymousFunction();
+};
